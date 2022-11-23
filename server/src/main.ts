4ce9c648da4d94ca92c3e25c203ca5c122771423
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-23 01:35:42 -0300

    Pus Swagger in the default route

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-doc', app, document);
+  SwaggerModule.setup('', app, document);
   
   await app.listen(process.env.PORT || 3000);
 }
